1098e9443ae010500d39dc30a710417e1b2aca4a
Christopher Carrasco <christopher3jesus@gmail.com>
2025-03-22 14:43:56 -0500

    feat: add extract data controller

--- controllers/extractData.js
+++ controllers/extractData.js
@@ -1,12 +1,32 @@
 const { request, response } = require("express");
 
-const extractDataPost = (req = request, res = response) => {
-  const { id } = req.body;
+const scraperService = require("../services/scraperService");
+const productsScraper = require("../services/productsScraper");
 
-  res.json({
-    msg: "extractDataPost",
-    id,
-  });
+const extractDataPost = async (req = request, res = response) => {
+  const { page } = req.body;
+
+  const url = `https://tottus.falabella.com.pe/tottus-pe/category/cat13380487/Despensa?page=${page}`;
+
+  try {
+    const products = await scraperService.scrape(
+      url,
+      async ({ page, data: url }) => {
+        return await productsScraper.scrapeProducts(page, url);
+      }
+    );
+
+    res.json({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      error: "Internal server error",
+    });
+  }
 };
 
 module.exports = {
