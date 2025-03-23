dd72e0b00b3c33099d4174ec6b419d517bacb338
Christopher Carrasco <christopher3jesus@gmail.com>
2025-03-23 18:18:07 -0500

    feat: add classify packaging controller

--- controllers/classifyPackaging.js
+++ controllers/classifyPackaging.js
@@ -1,12 +1,22 @@
 const { request, response } = require("express");
 
-const classifyPackagingPost = (req = request, res = response) => {
-  const { id } = req.body;
+const imageClassificationService = require("../services/imageClassificationService");
 
-  res.json({
-    msg: "classifyPackagingPost",
-    id,
-  });
+const classifyPackagingPost = async (req = request, res = response) => {
+  const { apiKey, imageUrl } = req.body;
+
+  const candidateLabels = ["rigid packaging", "flexible packaging"];
+
+  try {
+    const classificationResult = await imageClassificationService.classifyImage(
+      apiKey,
+      imageUrl,
+      candidateLabels
+    );
+    res.status(200).json({ success: true, result: classificationResult });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
 };
 
 module.exports = {
