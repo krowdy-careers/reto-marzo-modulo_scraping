5f61957d7373f85aaef8e96bb8745c48d3c10d37
Christopher Carrasco <christopher3jesus@gmail.com>
2025-03-23 23:38:51 -0500

    feat: add extract range controller

--- controllers/extractRange.js
+++ controllers/extractRange.js
@@ -1,12 +1,44 @@
 const { request, response } = require("express");
 
-const extractRangePost = (req = request, res = response) => {
-  const { id } = req.body;
+const scraperService = require("../services/scraperService");
+const productsScraper = require("../services/productsScraper");
 
-  res.json({
-    msg: "extractRangePost",
-    id,
-  });
+const extractRangePost = async (req = request, res = response) => {
+  const { startPage, endPage } = req.body;
+
+  try {
+    const allProducts = [];
+
+    const scrapePromises = [];
+    for (let page = startPage; page <= endPage; page++) {
+      const url = `https://tottus.falabella.com.pe/tottus-pe/category/cat13380487/Despensa?page=${page}`;
+
+      scrapePromises.push(
+        scraperService.scrape(url, async ({ page, data: url }) => {
+          const products = await productsScraper.scrapeProducts(page, url);
+          return products;
+        })
+      );
+    }
+
+    const results = await Promise.all(scrapePromises);
+
+    results.forEach((products) => {
+      allProducts.push(...products);
+    });
+
+    res.json({
+      success: true,
+      totalProducts: allProducts.length,
+      products: allProducts,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      error: "Internal server error",
+    });
+  }
 };
 
 module.exports = {
